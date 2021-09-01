b739802ef059d51d83c77c47c03a43f739a26eff
BrandonArgel <brandargel@gmail.com>
2021-09-01 17:09:03 -0500

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const morgan = require('morgan')
+
+// inicializacion
+const app = express()
+
+// settings
+app.set('port', process.env.PORT || 5000)
+
+// middlewares
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// routes
+app.use('/api/badges', require('./routes/badge'))
+
+// startin the server
+app.listen(app.get('port'), () => {
+	console.log(`Server run on http://localhost:${app.get('port')}`)
+})
